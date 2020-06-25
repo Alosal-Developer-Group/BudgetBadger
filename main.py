491f4b475939cfccc5fcca24f1b78bf7dfbb36ae
Scoot <scottseppi@gmail.com>
2020-06-24 19:44:00 -0700

    e

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import time
+# 3Debatable_Impeach_caiman_Mince7_Queasy_Sedge
+
+def UIfrontend():
+    print("1 - Edit Budget")
+    print("2 - Read Budget")
+    if input() == "1":
+        print("1 - I received money")
+        print("2 - I spent or lost money")
+        if input == "1":
+            editbudget("GAIN")
+        else:
+            editbudget("LOSS")
+    else:
+        readbudget()
+
+
+def editbudget(editstatus):
+    if editstatus ==
+
+
+def MathEngine():
+
+
+
+def DirEngine():
+    try:
+        os.chdir("Budget")
+        f = open("currentbudget.dll", "r")
+        budget = f.read()
+        f.close()
+    except:
+        setup()
+
+
+
+def setup():
+    os.mkdir("Budget")
+    os.chdir("Budget")
+    f = open("currentbudget.dll", "w")
+    f.write(input("enter current budget"))
+    f.close()
