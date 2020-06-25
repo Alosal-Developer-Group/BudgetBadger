12b50cf009cbbbf60a4b76da8b186af41dc7605a
Ian Botashev <yanbotashev@gmail.com>
2020-06-24 23:57:52 -0700

    Organized a bit

--- api/engine.py
+++ api/engine.py
@@ -4,22 +4,20 @@ from outUI import *
 
 # I added parameter "value". This parameter is to be edited.
 def MathEngine(editstatus, value):
-    if editstatus == "LOSS":
+    # Basically what happens here is that, i create a variable named "path.
+    # Then, i go ONE directory back than the current directory we're working in
+    # And then, i go to budget.
 
-        # Basically what happens here is that, i create a variable named "path.
-        # Then, i go ONE directory back than the current directory we're working in
-        # And then, i go to budget.
-        path = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "\\Budget\\currentbudget.dll"
-        f = open(path, "r")
-        currentbudget = f.read()
-        f.close()
+    # DID ONE MORE CHANGE, i move all of this behind the if statements. Makes everything cleaner.
+    path = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "\\Budget\\currentbudget.dll"
+    f = open(path, "r")
+    currentbudget = f.read()
+    f.close()
+
+    if editstatus == "LOSS":
         mathout = int(currentbudget) - value
         outUImath(mathout)
     else:
-        path = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "\\Budget\\currentbudget.dll"
-        f = open(path, "r")
-        currentbudget = f.read()
-        f.close()
 
         mathout = int(currentbudget) + value
         outUImath(mathout)
